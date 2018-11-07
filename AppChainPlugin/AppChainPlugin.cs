f4f14156db83b36b2498da0f3cd3342e789c17c3
Michael Yao <michaely2008@sina.com>
2018-11-07 12:19:21 +0800

    增加AppChainPlugin插件 侦听应用链的创建事件，并自动运行新创建的应用链

---
+++ AppChainPlugin/AppChainPlugin.cs
@@ -0,0 +1,83 @@
+﻿using Zoro.Ledger;
+using Zoro.Wallets;
+using Zoro.Cryptography.ECC;
+
+namespace Zoro.Plugins
+{
+    public class AppChainPlugin : Plugin
+    {
+        private Wallet wallet;
+        private ushort port = Settings.Default.Port;
+        private ushort wsport = Settings.Default.WsPort;
+        private bool saveJson = Settings.Default.SaveJson;
+
+        public AppChainPlugin(PluginManager pluginMgr)
+            : base(pluginMgr)
+        {
+            if (pluginMgr.System != ZoroSystem.Root)
+                return;
+
+            if (port > 0 || wsport > 0)
+            {
+                Blockchain.AppChainNofity += OnAppChainEvent;
+            }
+        }
+
+        public override void SetWallet(Wallet wallet)
+        {
+            this.wallet = wallet;
+        }
+
+        private void OnAppChainEvent(object sender, AppChainEventArgs args)
+        {
+            if (args.Method == "Create")
+            {
+                string hashString = args.State.Hash.ToString();
+                // 启动应用链
+                ZoroSystem.Root.StartAppChain(hashString, port, wsport);
+
+                bool startConsensus = false;
+
+                if (wallet != null)
+                {
+                    startConsensus = checkStartConsensus(args.State.StandbyValidators);
+
+                    if (startConsensus)
+                    {
+                        ZoroSystem.Root.StartAppChainConsensus(hashString, wallet);
+                    }
+                }
+
+                if (saveJson)
+                {
+                    AppChainSettings settings = new AppChainSettings(hashString, port, wsport, startConsensus);
+
+                    AppChainsSettings.Default.Chains.Add(hashString, settings);
+
+                    AppChainsSettings.Default.SaveJsonFile();
+                }
+
+                if (port > 0)
+                    port++;
+
+                if (wsport > 0)
+                    wsport++;
+            }
+        }
+
+        private bool checkStartConsensus(ECPoint[] Validators)
+        {
+            for (int i = 0; i < Validators.Length; i++)
+            {
+                WalletAccount account = wallet.GetAccount(Validators[i]);
+                if (account?.HasKey == true)
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+    }
+}
---
+++ AppChainPlugin/Settings.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Extensions.Configuration;
+using System.Reflection;
+
+namespace Zoro.Plugins
+{
+    internal class Settings
+    {
+        public ushort Port { get; }
+        public ushort WsPort { get; }
+        public bool SaveJson { get; }
+
+        public static Settings Default { get; }
+
+        static Settings()
+        {
+            Default = new Settings(Assembly.GetExecutingAssembly().GetConfiguration());
+        }
+
+        public Settings(IConfigurationSection section)
+        {
+            this.Port = ushort.Parse(section.GetSection("Port").Value);
+            this.WsPort = ushort.Parse(section.GetSection("WsPort").Value);
+            this.SaveJson = bool.Parse(section.GetSection("SaveJson").Value);
+        }
+    }
+}
